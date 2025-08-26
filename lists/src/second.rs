1cc4c5ed8cc9a2cccb9001f1b78d7d6594861f8a
Mark Bond <34947848+bondms@users.noreply.github.com>
2025-08-26 16:07:07 +0100

    Implement IterMut

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -95,6 +95,29 @@ impl<'a, T> Iterator for Iter<'a, T> {
   }
 }
 
+// IterMut
+
+pub struct IterMut<'a, T> {
+  next: Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T> {
+  pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+    IterMut { next: self.head.as_deref_mut() }
+  }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+  type Item = &'a mut T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.take().map(|node| {
+      self.next = node.next.as_deref_mut();
+      &mut node.elem
+    })
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::List;
@@ -182,6 +205,22 @@ mod test {
     assert_eq!(iter.next(), Some(&3));
     assert_eq!(iter.next(), Some(&2));
     assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), None);
+  }
+
+  #[test]
+  fn iter_mut() {
+    let mut list = List::new();
+
+    // Populate.
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.iter_mut();
+    assert_eq!(iter.next(), Some(&mut 3));
+    assert_eq!(iter.next(), Some(&mut 2));
+    assert_eq!(iter.next(), Some(&mut 1));
     assert_eq!(iter.next(), None);
   }
 }
