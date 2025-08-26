0a832fd103c1459a77eac3cc055d5420acd23c7e
Mark Bond <34947848+bondms@users.noreply.github.com>
2025-08-26 14:42:30 +0100

    Test Iter to exhaustion

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -182,6 +182,6 @@ mod test {
     assert_eq!(iter.next(), Some(&3));
     assert_eq!(iter.next(), Some(&2));
     assert_eq!(iter.next(), Some(&1));
-    // assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), None);
   }
 }
